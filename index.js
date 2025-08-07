8adb718c09dca4e12823b990ee7c423dd0d5c758
William Marlette <williammarlette3@gmail.com>
2025-08-07 11:15:40 -0400

    update entrypoint to have CORS + helment, use .env, errorhandler, ip limiter, and use routes' index

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
+const cors = require("cors");
+const helmet = require("helmet");
+const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const apiLimiter = require("./middlewares/rate-limiter");
+const allowedOrigins = [
+  process.env.LOCAL_ORIGIN,
+  ...process.env.PROD_ORIGINS.split(","),
+];
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_URI = "mongodb://localhost:27017/invare-db" } =
+  process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/invare-db")
+  .connect(MONGO_URI)
   .then(() => {
-    console.log("Connected to DB");
+    console.log(
+      `Connected to DB. Running on domains ${allowedOrigins.slice(1)}`,
+    );
   })
   .catch(console.error);
+
+app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log(`blocked CORS request from: ${origin}`);
+        callback(new Error("not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(requestLogger);
+app.use(apiLimiter);
+app.use("/", mainRouter);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
