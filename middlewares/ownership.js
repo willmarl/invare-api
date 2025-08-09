3bd39567d84d54b6e1419021a1a8cbf4e91abab0
William Marlette <williammarlette3@gmail.com>
2025-08-09 03:40:44 -0400

    add middleware to check if request made is by owner

---
+++ middlewares/ownership.js
@@ -0,0 +1,23 @@
+const ownershipCheck = (model, ownerField = "author") => {
+  return async (req, res, next) => {
+    try {
+      const resource = await model.findById(req.params.id);
+      if (!resource)
+        return res.status(404).json({ message: "Resource not found" });
+
+      const ownerId = resource[ownerField]?.toString();
+      if (ownerId !== req.user._id) {
+        return res
+          .status(403)
+          .json({ message: "Not authorized to modify this resource" });
+      }
+
+      req.resource = resource;
+      next();
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
+};
+
+module.exports = ownershipCheck;
